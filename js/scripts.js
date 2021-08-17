bc1ab70ebbe77040e636aa6e1a9189a948029c04
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-16 21:53:09 -0700

    Added 'required' option to the form input

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,32 @@
+function validateForm() {
+  event.preventDefault();
+  var a = document.forms["inputForm"]["item1"].value;
+  var b = document.forms["inputForm"]["item2"].value;
+  var c = document.forms["inputForm"]["item3"].value;
+  var d = document.forms["inputForm"]["item4"].value;
+  if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") {
+    alert("Please Fill All Required Field");
+    return false;
+  }
+}
+
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     $(".card-showing").hide();
-    // $(".card-hidden").show();
-    const shopArr = [
-        $("input#item1").val(), 
-        $("input#item2").val(), 
-        $("input#item3").val(), 
-        $("input#item4").val(), 
-        $("input#item5").val(), 
-        $("input#item6").val()
-    ];
 
+    const shopArr = [
+      $("input#item1").val(), 
+      $("input#item2").val(), 
+      $("input#item3").val(), 
+      $("input#item4").val(), 
+      $("input#item5").val(), 
+      $("input#item6").val()
+    ];
+    
     shopArr.sort();
-    shopArrCap = shopArr.map(item => item.toUpperCase());
+    let shopArrCap = shopArr.map(item => item.toUpperCase());
 
     let i = 1;
     shopArrCap.forEach(elem => {
